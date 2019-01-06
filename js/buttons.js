28cdfe372b38389c3352ce17e4d9057b39dddbb0
rchamber1 <rchamber@ucsd.edu>
2019-01-06 01:01:21 -0800

    Barebones Projects.

--- js/buttons.js
+++ js/buttons.js
@@ -2,20 +2,22 @@ $( document ).ready(function() {
 
     console.log("jQuery working!");
 
-    var toggle = 0;
     $(".design-projects").hide();
+    var toggle = 1;
 
     $("#toggle-btn").click(function() {
-        if(toggle > 0) {
-            $(".design-projects").hide();
-            $(".technical-projects").show();
-            toggle = 0;
-        }
-        else {
+
+        if(toggle>0){
             $(".design-projects").show();
             $(".technical-projects").hide();
-            toggle = 1;
+            toggle=0;
         }
-
+        else{
+            $(".technical-projects").show();
+            $(".design-projects").hide();
+            toggle=1;
+        }
+        $(".intro").hide();
     });
+
 });
