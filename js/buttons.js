e440d5cfa62cf3d7f32b35615a0b2b47959ad315
rchamber1 <rchamber@ucsd.edu>
2019-01-05 22:54:50 -0800

    Basic transition functionality.

--- js/buttons.js
+++ js/buttons.js
@@ -1,19 +1,21 @@
-$(document).ready(() => {
+$( document ).ready(function() {
+
     console.log("jQuery working!");
 
     var toggle = 0;
+    $(".design-projects").hide();
 
-    $("#toggle-btn").click(() => {
-
-        if(toggle > 0){
+    $("#toggle-btn").click(function() {
+        if(toggle > 0) {
             $(".design-projects").hide();
             $(".technical-projects").show();
             toggle = 0;
         }
-        else{
+        else {
             $(".design-projects").show();
             $(".technical-projects").hide();
             toggle = 1;
         }
+
     });
 });
